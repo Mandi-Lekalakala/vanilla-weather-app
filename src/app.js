d0f69fa2acca15f3b22daee7c23ec91b77e3b3ff
Mandi-Lekalakala <mandilekalakala@gmail.com>
2025-11-12 16:18:04 +0200

    added search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+  let cityValue = response.data.city;
+  cityElement.innerHTML = cityValue;
+}
+
+function searchCity(city) {
+  let apiKey = "cfabea60f0eoe0t3bb3e16a70e85e14b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Polokwane");
